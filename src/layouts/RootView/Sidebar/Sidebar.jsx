45f0c429eff09230cc1a7991121e02a3ecb2216c
nickau309 <nickau309@gmail.com>
2023-06-15 19:51:45 +0800

    Optimize style

--- src/layouts/RootView/Sidebar/Sidebar.jsx
+++ src/layouts/RootView/Sidebar/Sidebar.jsx
@@ -18,8 +18,8 @@ export default function Sidebar({ showSidebar }) {
       }}
       className={classNames(
         "group/sidebar flex h-full w-[305px] flex-col gap-4 overflow-y-auto overflow-x-hidden bg-aside px-[18px] pt-[18px]",
-        showSidebar &&
-          "max-[751px]:shadow-[0_2px_10px_rgba(0,0,0,.3)] min-[751px]:pt-[30px]"
+        showSidebar && "max-[751px]:shadow-[0_2px_10px_rgba(0,0,0,.3)]",
+        "min-[751px]:pt-[30px]"
       )}
     >
       <ul>
